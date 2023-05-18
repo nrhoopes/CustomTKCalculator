a0755f12f1cbbf8abc6a1bee5c5846ee31abf174
nrhoopes <nrhoopes1@gmail.com>
2023-05-18 16:47:30 -0400

    Documentation and Clear Button

--- src/view/CalcGUI.py
+++ src/view/CalcGUI.py
@@ -27,54 +27,58 @@ class CalcGUI():
     # Populates the main window with widget for a basic calculator
     def basicCalc(self):
         self.buttonWidth = 75
+        self.fontSize = 35
 
         # Creation of the operation box at the top of the window.
-        self.opBox = ctk.CTkEntry(self.mainFrame, height=75, width=350, justify="right", font=("TkDefaultFont", 30))
+        self.opBox = ctk.CTkEntry(self.mainFrame, height=75, width=350, justify="right", font=("TkDefaultFont", self.fontSize))
         self.opBox.grid(column=0, row=0, columnspan=4, pady=5)
 
-        
-        self.subButton = ctk.CTkButton(self.mainFrame, text="-", width=self.buttonWidth, font=("TkDefaultFont", 30))
+        # Creation of the operation buttons along the top and side of keypad
+        self.subButton = ctk.CTkButton(self.mainFrame, text="-", width=self.buttonWidth, font=("TkDefaultFont", self.fontSize))
         self.subButton.grid(column=3, row=1, pady=5)
 
-        self.multButton = ctk.CTkButton(self.mainFrame, text="*", width=self.buttonWidth, font=("TkDefaultFont", 30))
+        self.multButton = ctk.CTkButton(self.mainFrame, text="*", width=self.buttonWidth, font=("TkDefaultFont", self.fontSize))
         self.multButton.grid(column=2, row=1, pady=5)
 
-        self.divideButton = ctk.CTkButton(self.mainFrame, text="/", width=self.buttonWidth * 2 + 10, font=("TkDefaultFont", 30))
-        self.divideButton.grid(column=0, row=1, columnspan=2, pady=5)
+        self.divideButton = ctk.CTkButton(self.mainFrame, text="/", width=self.buttonWidth, font=("TkDefaultFont", self.fontSize))
+        self.divideButton.grid(column=1, row=1, pady=5)
 
-        self.addButton = ctk.CTkButton(self.mainFrame, text="+", width=self.buttonWidth, font=("TkDefaultFont", 30))
+        self.addButton = ctk.CTkButton(self.mainFrame, text="+", width=self.buttonWidth, font=("TkDefaultFont", self.fontSize))
         self.addButton.grid(column=3, row=2, rowspan=2, pady=5, sticky="ns")
 
-        self.equalButton = ctk.CTkButton(self.mainFrame, text="=", width=self.buttonWidth, font=("TkDefaultFont", 30))
+        self.equalButton = ctk.CTkButton(self.mainFrame, text="=", width=self.buttonWidth, font=("TkDefaultFont", self.fontSize))
         self.equalButton.grid(column=3, row=4, rowspan=2, pady=5, sticky="ns")
 
+        self.clearButton = ctk.CTkButton(self.mainFrame, text="CE", width=self.buttonWidth, font=("TkDefaultFont", self.fontSize))
+        self.clearButton.grid(column=0, row=1, pady=5)
 
-        self.sevenButton = ctk.CTkButton(self.mainFrame, text="7", width=self.buttonWidth, font=("TkDefaultFont", 30))
+        # Creation of the number buttons on the keypad 0-9
+        self.sevenButton = ctk.CTkButton(self.mainFrame, text="7", width=self.buttonWidth, font=("TkDefaultFont", self.fontSize))
         self.sevenButton.grid(column=0, row=2, pady=5)
 
-        self.eightButton = ctk.CTkButton(self.mainFrame, text="8", width=self.buttonWidth, font=("TkDefaultFont", 30))
+        self.eightButton = ctk.CTkButton(self.mainFrame, text="8", width=self.buttonWidth, font=("TkDefaultFont", self.fontSize))
         self.eightButton.grid(column=1, row=2, pady=5)
 
-        self.nineButton = ctk.CTkButton(self.mainFrame, text="9", width=self.buttonWidth, font=("TkDefaultFont", 30))
+        self.nineButton = ctk.CTkButton(self.mainFrame, text="9", width=self.buttonWidth, font=("TkDefaultFont", self.fontSize))
         self.nineButton.grid(column=2, row=2, pady=5)
 
-        self.fourButton = ctk.CTkButton(self.mainFrame, text="4", width=self.buttonWidth, font=("TkDefaultFont", 30))
+        self.fourButton = ctk.CTkButton(self.mainFrame, text="4", width=self.buttonWidth, font=("TkDefaultFont", self.fontSize))
         self.fourButton.grid(column=0, row=3, pady=5)
 
-        self.fiveButton = ctk.CTkButton(self.mainFrame, text="5", width=self.buttonWidth, font=("TkDefaultFont", 30))
+        self.fiveButton = ctk.CTkButton(self.mainFrame, text="5", width=self.buttonWidth, font=("TkDefaultFont", self.fontSize))
         self.fiveButton.grid(column=1, row=3, pady=5)
 
-        self.sixButton = ctk.CTkButton(self.mainFrame, text="6", width=self.buttonWidth, font=("TkDefaultFont", 30))
+        self.sixButton = ctk.CTkButton(self.mainFrame, text="6", width=self.buttonWidth, font=("TkDefaultFont", self.fontSize))
         self.sixButton.grid(column=2, row=3, pady=5)
 
-        self.oneButton = ctk.CTkButton(self.mainFrame, text="1", width=self.buttonWidth, font=("TkDefaultFont", 30))
+        self.oneButton = ctk.CTkButton(self.mainFrame, text="1", width=self.buttonWidth, font=("TkDefaultFont", self.fontSize))
         self.oneButton.grid(column=0, row=4, pady=5)
 
-        self.twoButton = ctk.CTkButton(self.mainFrame, text="2", width=self.buttonWidth, font=("TkDefaultFont", 30))
+        self.twoButton = ctk.CTkButton(self.mainFrame, text="2", width=self.buttonWidth, font=("TkDefaultFont", self.fontSize))
         self.twoButton.grid(column=1, row=4, pady=5)
 
-        self.threeButton = ctk.CTkButton(self.mainFrame, text="3", width=self.buttonWidth, font=("TkDefaultFont", 30))
+        self.threeButton = ctk.CTkButton(self.mainFrame, text="3", width=self.buttonWidth, font=("TkDefaultFont", self.fontSize))
         self.threeButton.grid(column=2, row=4, pady=5)
 
-        self.zeroButton = ctk.CTkButton(self.mainFrame, text="0", width=self.buttonWidth, font=("TkDefaultFont", 30))
+        self.zeroButton = ctk.CTkButton(self.mainFrame, text="0", width=self.buttonWidth, font=("TkDefaultFont", self.fontSize))
         self.zeroButton.grid(column=1, row=5, pady=5)
