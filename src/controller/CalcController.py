0bbb995f27ed50b243aadc20642f72ca560690b9
nrhoopes <nrhoopes1@gmail.com>
2023-06-25 14:10:23 -0400

    Updated percent function

--- src/controller/CalcController.py
+++ src/controller/CalcController.py
@@ -10,9 +10,21 @@ class calcController:
     # Will accept a number, convert it to a percentage and then will return the percentage
     # and place it into the operation box.
     def convertPercent(self, number):
+        percentDec = ""
+        for i in range(len(number) - 1, -1, -1):
+            if number[i].isnumeric():
+                percentDec = number[i] + percentDec
+                number = number[0:len(number)-1]
+            else:
+                break
         self.ui.clearScreen()
-        self.ui.insertToOpBox(float(number)/100)
+        self.ui.insertToOpBox(number + str(float(percentDec)/100))
 
+    # Public method evaluate
+    # Arguments:
+    #   - expression: an expression that is input into the calculator in string form.
+    #
+    # Will evaluate the expression, cast it to a string, and then tell the opBox to display that string.
     def evaluate(self, expression):
         evaluation = str(eval(expression))
         self.ui.displayToOpBox(evaluation)
